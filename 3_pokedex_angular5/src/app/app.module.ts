a619c27c8e37e2aa1743afae49f0f9006d3c8772
Desarrollo1\Desarrollador1 <slondono@gisolutions.com.co>
2018-04-09 21:40:31 -0500

    Migracion compass - Angular

--- 3_pokedex_angular5/src/app/app.module.ts
+++ 3_pokedex_angular5/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
